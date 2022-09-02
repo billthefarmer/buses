1b60fae9aa0f3f84115c1333cdf6929ab887f496
Bill Farmer <williamjfarmer@yahoo.co.uk>
2022-09-02 10:54:47 +0100

    Update docs

--- src/main/java/org/billthefarmer/buses/Buses.java
+++ src/main/java/org/billthefarmer/buses/Buses.java
@@ -396,6 +396,7 @@ public class Buses extends Activity
             if (location != null)
                 showLocation(location);
 
+            locationManager.requestSingleUpdate(provider, listener, null);
             locationManager
                 .requestLocationUpdates(LocationManager.GPS_PROVIDER,
                                         SHORT_DELAY, 0, listener);
