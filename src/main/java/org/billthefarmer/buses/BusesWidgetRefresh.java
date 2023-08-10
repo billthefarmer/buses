13911cd0ad1ddfa76dda6dbb99d59fa59f28532b
Bill Farmer <williamjfarmer@yahoo.co.uk>
2023-08-10 12:18:32 +0100

    Update version, changelog

--- src/main/java/org/billthefarmer/buses/BusesWidgetRefresh.java
+++ src/main/java/org/billthefarmer/buses/BusesWidgetRefresh.java
@@ -25,10 +25,7 @@ package org.billthefarmer.buses;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 // BusesWidgetRefresh
